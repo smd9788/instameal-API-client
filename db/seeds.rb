da39fa61a1f8b611b1f97ae635cc3899b1cc5540
smd9788 <smd09876@gmail.com>
2019-01-30 14:03:32 -0500

    Seeded meals data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database
-# with its default values.
-# The data can then be loaded with the rails db:seed (or created alongside the
-# db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Meal.delete_all
+Meal.create! id: 1, name: 'Chicken Parmesean', price: 18.99
+Meal.create! id: 2, name: 'Filet Mignon', price: 38.99
+Meal.create! id: 3, name: 'Caesar Salad', price: 8.99
+Meal.create! id: 4, name: 'Fish Tacos', price: 14.99
+Meal.create! id: 5, name: 'Strawberry Cheesecake', price: 5.99
